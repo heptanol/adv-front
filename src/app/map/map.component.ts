41adf57551c8b9eaa92f356a79452624a22fc7ae
Alaeddine Thamine <athamine@maltem.com>
2017-11-22 17:50:14 +0100

    Mettre à jour le user page

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component,  OnInit } from '@angular/core';
+import { Component,  OnInit, Input } from '@angular/core';
 import { LatLngBounds, MapsAPILoader } from '@agm/core';
 import {AuthenticationService} from '../authentication/authentication.service';
 import {UserRepositoryService} from '../user/user-repository.service';
